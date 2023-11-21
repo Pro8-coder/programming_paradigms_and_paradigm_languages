050bfa7363e82dd1609af350ea42f1585da016da
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2023-11-21 07:18:18 +0300

    Pro8

---
+++ home_work_01/Task_1.py
@@ -0,0 +1,15 @@
+# Задача №1
+# Дан список целых чисел numbers. Необходимо написать в императивном стиле процедуру для
+# сортировки числа в списке в порядке убывания. Можно использовать любой алгоритм сортировки.
+
+def sort_list_imperative(numbers):
+    n = len(numbers)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if numbers[j] < numbers[j+1]:
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+    
+    return numbers
+
+
+print(sort_list_imperative([5, 6, 1, 7, 2, 5]))
---
+++ home_work_01/Task_2.py
@@ -0,0 +1,10 @@
+# Задача №2
+# Написать точно такую же процедуру, но в декларативном стиле
+
+def sort_list_declarative(numbers):
+    numbers.sort(reverse=True)
+
+
+lst = [5, 6, 1, 7, 2, 5]
+sort_list_declarative(lst)
+print()
